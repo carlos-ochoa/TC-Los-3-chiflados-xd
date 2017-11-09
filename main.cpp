25d61bdcd66883fbb9057fcf0c5872e88f00bcb6
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-11-09 09:54:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
 https://codeshare.io/5oLz0M
+
+
+#include<iostream>
+#include<string>
+#include"TADPilaDin.h"
+
+using namespace std;
+
+int main(void){
+  string cadena;
+  elemento tope;
+  elemento alf_pila;
+  pila expresion;
+  Initialize(&expresion);
+  cout << "Escriba por favor la cadena a evaluar" << endl;
+  cin >> cadena;
+  for(int i = 0; i < cadena.length() ; i++){
+    if(cadena.at(i) == "a" && Top(&expresion).c == "Z0"){
+      alf_pila.s = "A";
+      Push(&expresion, alf_pila);
+    }
+    else if(cadena.at(i) == "b" && Top(&expresion).c == "Z0"){
+      alf_pila.s = "B";
+   		Push(&expresion, alf_pila); 
+    }
+    else if(cadena.at(i) == "a" && Top(&expresion).c == "A"){
+      alf_pila.s = "A";
+   		Push(&expresion, alf_pila);
+    }
+    else if(cadena.at(i) == "b" && Top(&expresion).c == "B"){
+      alf_pila.s = "B";
+   		Push(&expresion, alf_pila);
+    }
+    else if(cadena.at(i) == "a" && Top(&expresion).c == "B"){
+           Pop(&expresion);
+    }
+    else if(cadena.at(i) == "b" && Top(&expresion).c == "A"){
+           Pop(&expresion);
+    }
+    else {
+      
+      cout << "La cadena no es válida en este lenguaje" << endl;
+      break;
+    }
+  }
+	  return 0;
+}
