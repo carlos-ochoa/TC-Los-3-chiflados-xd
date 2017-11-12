e030d7dd2999c100507aa1bb35415ed14dfdd1a1
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-11-12 15:38:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-https://codeshare.io/5oLz0M
-
-
 #include<iostream>
 #include<string>
 #include"TADPilaDin.h"
@@ -9,40 +6,84 @@ using namespace std;
 
 int main(void){
   string cadena;
-  elemento tope;
+  int estado;
+  int i;
+  bool continuar = true;
   elemento alf_pila;
   pila expresion;
-  Initialize(&expresion);
-  cout << "Escriba por favor la cadena a evaluar" << endl;
-  cin >> cadena;
-  for(int i = 0; i < cadena.length() ; i++){
-    if(cadena.at(i) == "a" && Top(&expresion).c == "Z0"){
-      alf_pila.s = "A";
-      Push(&expresion, alf_pila);
-    }
-    else if(cadena.at(i) == "b" && Top(&expresion).c == "Z0"){
-      alf_pila.s = "B";
-   		Push(&expresion, alf_pila); 
-    }
-    else if(cadena.at(i) == "a" && Top(&expresion).c == "A"){
-      alf_pila.s = "A";
-   		Push(&expresion, alf_pila);
-    }
-    else if(cadena.at(i) == "b" && Top(&expresion).c == "B"){
-      alf_pila.s = "B";
-   		Push(&expresion, alf_pila);
-    }
-    else if(cadena.at(i) == "a" && Top(&expresion).c == "B"){
-           Pop(&expresion);
-    }
-    else if(cadena.at(i) == "b" && Top(&expresion).c == "A"){
-           Pop(&expresion);
-    }
-    else {
-      
-      cout << "La cadena no es válida en este lenguaje" << endl;
+  while(continuar){
+    Initialize(&expresion);
+    i = 0;
+    estado = 0;
+    cout << "\nEscriba por favor la cadena a evaluar" << endl;
+    cout << "Si desea salir presione cero" << endl;
+    cin >> cadena;
+    if(cadena.at(i) == '0')
       break;
+	  do{
+	    switch(estado){
+	      case 0:
+		cout << "\nEstado 0" << endl;
+		if(cadena.at(i) == 'a' && Top(&expresion).c == "Z0"){
+		  estado = 1;
+		  alf_pila.c = "A";
+		  cout << "Z0a|Z0A" << endl;
+		  Push(&expresion, alf_pila);
+		}
+		else if(cadena.at(i) == 'b' && Top(&expresion).c == "Z0"){
+		  estado = 1;
+		  alf_pila.c = "B";
+		  cout << "Z0b|Z0B" << endl;
+		  Push(&expresion, alf_pila); 
+		}
+		else{
+		  cout << "No se contempla esta regla en el estado actual" << endl;
+		  i = cadena.length();
+		}
+		break;
+	      case 1:
+		cout << "\nEstado 1" << endl;
+		//Reglas encontradas en el estado uno
+		if(cadena.at(i) == 'a' && Top(&expresion).c == "A"){
+		  estado = 1;
+		  alf_pila.c = "A";
+		  cout << "Aa|AA" << endl;
+		  Push(&expresion, alf_pila);
+		}
+		else if(cadena.at(i) == 'b' && Top(&expresion).c == "B"){
+		  estado = 1;
+		  alf_pila.c = "B";
+		  cout << "Bb|BB" << endl;
+		  Push(&expresion, alf_pila);
+		}
+		else if(cadena.at(i) == 'a' && Top(&expresion).c == "B"){
+		  estado = 1;
+		  cout << "Ba|ε" << endl;
+		  Pop(&expresion);
+		}
+		else if(cadena.at(i) == 'b' && Top(&expresion).c == "A"){
+		  estado = 1;
+		  cout << "Ab|ε" << endl;
+		  Pop(&expresion);
+		} else {
+		  cout << "No se contempla esta regla en el estado actual" << endl;
+		  i = cadena.length();
+		}
+		break;
+	    }
+	    i++;
+	  }while(i < cadena.length());
+	  if(i == cadena.length() && Top(&expresion).c == "Z0")
+	    estado = 0;
+	  if(Top(&expresion).c == "Z0" && estado == 0){
+	    cout << "\nLa expresión dada es válida en este lenguaje\n" << endl;
+            Destroy(&expresion);
+          }
+	  else{
+	    cout << "\nLa expresión no es válida" << endl;
+	    cout << "Lo que se quedó en el tope de la pila es " << Top(&expresion).c << endl;
+            Destroy(&expresion);
+	  }
     }
-  }
 	  return 0;
 }
